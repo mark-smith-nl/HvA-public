e4a0579d87f7aa0e11ce41bdfc0a397996523ece
Mark Smith <mark_smith_nl@yahoo.co.uk>
2021-04-21 15:07:33 +0200

    Example Stream.generate()

--- src/main/java/hva/ads/practicum/week9/StreamExamples.java
+++ src/main/java/hva/ads/practicum/week9/StreamExamples.java
@@ -11,11 +11,10 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 /**
- * This method <description of functionality>
+ * Examples using Stream.generate()
  *
  * @author m.smithhva.nl
  */
