9a8014424aa6af02306aa73bb29633174e4e28ea
Mark Smith <mark_smith_nl@yahoo.co.uk>
2021-05-20 09:59:43 +0200

    Stream example using Stream.of()

---
+++ src/main/java/hva/ads/college/week08/StreamExample.java
@@ -0,0 +1,24 @@
+package hva.ads.college.week08;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+
+/**
+ * This method <description of functionality>
+ *
+ * @author m.smithhva.nl
+ */
+public class StreamExample {
+
+    public static void main(String[] args) {
+        List<String> strings = Arrays.asList("Mark", "Tom", "Frank", "Petra");
+       // strings.stream().map(String::toUpperCase).filter()
+
+    }
+}
